f830c144314db727e0f2f26ced9562a669ff7a96
jun-hy <skdlvm0@gmail.com>
2020-06-11 20:34:08 +0900

    chatting dev update

---
+++ app/src/main/java/com/example/kakao_clone/Chat.java
@@ -0,0 +1,25 @@
+package com.example.kakao_clone;
+
+public class Chat {
+
+    private String Name;
+    private String chat;
+
+    public Chat() {}
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getChat() {
+        return chat;
+    }
+
+    public void setChat(String chat) {
+        this.chat = chat;
+    }
+}
