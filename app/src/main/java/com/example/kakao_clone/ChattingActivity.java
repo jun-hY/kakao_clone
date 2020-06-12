6a13f68a0c4590f7d26d91f2ae1ec52d9495e645
jun-hy <skdlvm0@gmail.com>
2020-06-12 14:43:01 +0900

    chatting dev update

--- app/src/main/java/com/example/kakao_clone/ChattingActivity.java
+++ app/src/main/java/com/example/kakao_clone/ChattingActivity.java
@@ -1,15 +1,35 @@
 package com.example.kakao_clone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class ChattingActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<Chat> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
+
+        recyclerView.findViewById(R.id.chtscr);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>();
+
     }
 
 }
