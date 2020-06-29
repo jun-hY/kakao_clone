a5080b447871c377e95adc65aa089509be7a8339
pescecanekim <a053311@naver.com>
2020-06-29 19:55:26 +0900

    password_check

--- app/src/main/java/com/example/kakao_clone/SignupActivity.java
+++ app/src/main/java/com/example/kakao_clone/SignupActivity.java
@@ -23,6 +23,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     //define view objects
     EditText editTextEmail;
     EditText editTextPassword;
+    EditText getEditTextPasswordCheck;
     Button buttonSignup;
     TextView textviewSingin;
     TextView textviewMessage;
@@ -42,6 +43,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         //initializing views
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        getEditTextPasswordCheck = (EditText) findViewById(R.id.editTextPasswordCheck);
         textviewSingin= (TextView) findViewById(R.id.textViewSignin);
         textviewMessage = (TextView) findViewById(R.id.textviewMessage);
         buttonSignup = (Button) findViewById(R.id.buttonSignup);
@@ -59,6 +61,8 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         //사용자가 입력하는 email, password를 가져온다.
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
+        String passwordCheck = getEditTextPasswordCheck.getText().toString().trim();
+
         //email과 password가 비었는지 아닌지를 체크 한다.
         if(TextUtils.isEmpty(email)){
             Toast.makeText(this, "Email을 입력해 주세요.", Toast.LENGTH_SHORT).show();
@@ -67,28 +71,37 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
         if(TextUtils.isEmpty(password)){
             Toast.makeText(this, "Password를 입력해 주세요.", Toast.LENGTH_SHORT).show();
         }
+        if(TextUtils.isEmpty(passwordCheck)){
+            Toast.makeText(this, "Password 확인을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+        }
 
         //email과 password가 제대로 입력되어 있다면 계속 진행된다.
         progressDialog.setMessage("등록중입니다. 기다려 주세요...");
         progressDialog.show();
 
         //creating a new user
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
-                            finish();
-                        } else {
-                            //에러발생시
-                            textviewMessage.setText("에러유형\n - 이미 등록된 이메일  \n -암호 최소 6자리 이상 \n - 서버에러");
-                            Toast.makeText(SignupActivity.this, "등록 에러!", Toast.LENGTH_SHORT).show();
+        if(password.equals(passwordCheck)) {
+            firebaseAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+                                finish();
+                            }
+                            else {
+                                //에러발생시
+                                textviewMessage.setText("에러유형\n - 이미 등록된 이메일  \n -암호 최소 6자리 이상 \n - 서버에러");
+                                Toast.makeText(SignupActivity.this, "등록 에러!", Toast.LENGTH_SHORT).show();
+                            }
+                            progressDialog.dismiss();
                         }
-                        progressDialog.dismiss();
-                    }
-                });
-
+                    });
+        }
+        else {
+            textviewMessage.setText("에러유형\n - 비밀번호 확인을 하세요. \n - 서버에러");
+            progressDialog.dismiss();
+        }
     }
 
     //button click event
