f23cb10968b8ad3844eb523412abefeca7d8491a
jun-hy <skdlvm0@gmail.com>
2020-06-30 20:15:41 +0900

    chatting dev update

--- app/src/main/java/com/example/kakao_clone/ChatAdapter.java
+++ app/src/main/java/com/example/kakao_clone/ChatAdapter.java
@@ -1,65 +1,7 @@
 package com.example.kakao_clone;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-
-import java.util.ArrayList;
-
-public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
-
-    private ArrayList<Chat> arrayList;
-    private Context context;
-
-    public ChatAdapter(ArrayList<Chat> arrayList, Context context) {
-        this.arrayList = arrayList;
-        this.context = context;
-    }
-
-    @NonNull
-    @Override
-    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_chat, parent, false);
-        ChatViewHolder holder = new ChatViewHolder(view);
-
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
-        Glide.with(holder.itemView)
-                .load(arrayList.get(position).getProfile())
-                .into(holder.them_profile);
-        holder.UserName.setText(arrayList.get(position).getName());
-        holder.chatCon.setText(arrayList.get(position).getChat());
-    }
-
-    @Override
-    public int getItemCount() {
-        return (arrayList != null ? arrayList.size() : 0);
-    }
-
-    public class ChatViewHolder extends RecyclerView.ViewHolder {
-        ImageView them_profile;
-        TextView chatCon;
-        TextView UserName;
-
-        public ChatViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            this.them_profile = itemView.findViewById(R.id.them_profile);
-            this.chatCon = itemView.findViewById(R.id.chatCon);
-            this.UserName = itemView.findViewById(R.id.UserName);
-        }
-
-    }
+public class ChatAdapter extends FirestoreRecyclerAdapter<Chat, ChatAdapter.ChatHolder> {
 
 }
