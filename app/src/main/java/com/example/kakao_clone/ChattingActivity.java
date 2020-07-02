8638dbe5460ff1ea82739e074f2604b50fee3b30
jun-hy <skdlvm0@gmail.com>
2020-07-02 23:08:59 +0900

    Update ChattingActivity.java

--- app/src/main/java/com/example/kakao_clone/ChattingActivity.java
+++ app/src/main/java/com/example/kakao_clone/ChattingActivity.java
@@ -5,51 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class ChattingActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
 
-    FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
-    mUser.getIdToken(true)
-            .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
-        public void onComplete(@NonNull Task<GetTokenResult> task) {
-            if (task.isSuccessful()) {
-                String idToken = task.getResult().getToken();
-                // Send token to your backend via HTTPS
-                // ...
-            } else {
-                // Handle error -> task.getException();
-            }
-        }
-    });
-
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private CollectionReference ChatRef = db.collection("messages");
+    private CollectionReference ChatRef = db.collection("Users")
+            .document(Uid).collection("rooms")
+            .document(Chatroom).collection("messages");
 
     private ChatAdapter adapter;
 
