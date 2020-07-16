7da6734e52d318c53f7f0282edce443a7eeba173
jun-hy <skdlvm0@gmail.com>
2020-07-16 23:29:32 +0900

    adsf

--- app/src/main/java/com/example/kakao_clone/ChattingActivity.java
+++ app/src/main/java/com/example/kakao_clone/ChattingActivity.java
@@ -7,19 +7,31 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.LogWriter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -27,6 +39,9 @@ public class ChattingActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
     private CollectionReference ChatRef;
+
+    private DocumentReference userRef;
+
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
 
@@ -34,6 +49,8 @@ public class ChattingActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private String uid;
     private Date date;
+
+    private Collection userInfo;
 
     private static final String TAG = "ChattingActivity";
 
@@ -48,15 +65,33 @@ public class ChattingActivity extends AppCompatActivity {
 
         db = FirebaseFirestore.getInstance();
         ChatRef = db.collection("Users")
-                .document("asdfqw@gmail.com").collection("rooms")
+                .document(uid).collection("rooms")
                 .document("asdf").collection("messages");
+        userRef = db.collection("Users")
+                .document(uid).collection("rooms")
+                .document("asdf");
+
+        userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    if (documentSnapshot.exists()) {
+                        Log.d(TAG, "onComplete: " + documentSnapshot.getData());
+                        userInfo = documentSnapshot.getData().values();
+                    } else {
+                        Log.d(TAG, "Not Such Data");
+                    }
+                } else {
+                    Log.d(TAG, "Get Failed with"  + task.getException());
+                }
+            }
+        }).;
+
+
 
         final EditText sendTxt = findViewById(R.id.sendtxt);
         final ImageButton sendBtn = findViewById(R.id.sendbtn);
-
-
-
-
 
         setUpRecyclerView();
 
